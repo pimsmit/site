6cf5b01db94c4aaf31ac0748ba294551cbd7cd57
Ainomiq <info@ainomiq.com>
2026-04-13 15:19:51 +0200

    Fix tablet section: more space for text, smoother scroll animation

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -37,10 +37,10 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[60rem] flex items-center justify-center relative p-8 md:p-12"
+      className="h-[70rem] flex items-center justify-center relative p-8 md:p-12"
       ref={containerRef}
     >
-      <div className="py-16 md:py-20 w-full relative" style={{ perspective: "1000px" }}>
+      <div className="py-20 md:py-24 w-full relative" style={{ perspective: "1000px" }}>
         <Header translate={translate} titleComponent={titleComponent} />
         <Card rotate={rotate} translate={translate} scale={scale}>
           {children}
@@ -175,7 +175,7 @@ export const Card = ({
         boxShadow:
           "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
-      className="max-w-5xl -mt-12 mx-auto h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] shadow-2xl"
+      className="max-w-5xl -mt-24 mx-auto h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] shadow-2xl"
     >
       <div className="h-full w-full overflow-hidden rounded-2xl bg-white">
         {children}
