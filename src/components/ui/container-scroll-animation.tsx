b80ddfc3b61e748488ac62ece8f670d35251fcb8
Ainomiq <info@ainomiq.com>
2026-04-14 12:46:06 +0200

    Smoother shadow fade: larger blur radii on outer layers

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -178,7 +178,7 @@ export const Card = ({
         rotateX: rotate,
         scale,
         boxShadow:
-          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003, 0 340px 120px #00000002",
+          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 80px #00000008, 0 233px 120px #00000004, 0 350px 180px #00000002",
       }}
       className="max-w-5xl -mt-24 mx-auto h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] relative z-30"
     >
