2b42912b88f86ae3d4f308504da650aea9a59f94
Ainomiq <info@ainomiq.com>
2026-04-14 12:28:20 +0200

    Restore tablet shadow + remove overflow-hidden

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -42,7 +42,7 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[70rem] flex items-center justify-center relative p-8 overflow-hidden"
+      className="h-[70rem] flex items-center justify-center relative p-8"
       ref={containerRef}
     >
       <div className="py-2 w-full relative" style={{ perspective: "1000px" }}>
@@ -178,7 +178,7 @@ export const Card = ({
         rotateX: rotate,
         scale,
         boxShadow:
-          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a",
+          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
       }}
       className="max-w-5xl -mt-24 mx-auto h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] shadow-2xl relative z-30"
     >
