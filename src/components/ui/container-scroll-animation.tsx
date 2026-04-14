f46925e593d1a87bf8ad0d918f4267f5840f3a4d
Ainomiq <info@ainomiq.com>
2026-04-14 12:44:31 +0200

    Fix: remove gradient div, extend box-shadow for smoother fade

--- src/components/ui/container-scroll-animation.tsx
+++ src/components/ui/container-scroll-animation.tsx
@@ -177,22 +177,14 @@ export const Card = ({
       style={{
         rotateX: rotate,
         scale,
+        boxShadow:
+          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003, 0 340px 120px #00000002",
       }}
-      className="max-w-5xl -mt-24 mx-auto relative z-30"
+      className="max-w-5xl -mt-24 mx-auto h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] relative z-30"
     >
-      <div
-        className="h-[40rem] w-full border-4 border-[#6C6C6C] p-6 bg-[#222222] rounded-[30px] relative"
-        style={{
-          boxShadow:
-            "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
-        }}
-      >
-        <div className="h-full w-full overflow-hidden rounded-2xl bg-white">
-          {children}
-        </div>
+      <div className="h-full w-full overflow-hidden rounded-2xl bg-white">
+        {children}
       </div>
-      {/* Slow gradient fade-out below tablet shadow */}
-      <div className="w-[90%] mx-auto h-40 bg-gradient-to-b from-black/[0.04] via-black/[0.01] to-transparent pointer-events-none" />
     </motion.div>
   );
 };
